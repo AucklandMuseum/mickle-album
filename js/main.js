dd8e8f92877b64389efd6306da4b891f5d80f09c
gdewalters <gdewalters@aucklandmuseum.com>
2016-02-21 17:08:13 +1300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+* main.js V0.0.1
+* Copyright 2016, Auckland War Memorial Museum
+* www.aucklandmuseum.com
+* Free to use under the Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+* http://creativecommons.org/licenses/by-sa/4.0/
+* 26/01/2016
+*
+* Please see vendors' licence for terms of third party scripts
+*/
